53888a71a35d18b1698dbfb3fd313d53c8ced4f6
Juan Manuel Oliú <juan@xmartlabs.com>
2021-11-18 12:11:01 -0300

    adds more problems

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,15 @@
-import { uniqueArray } from '.'
+import {
+  decimalToBinary,
+  isIsomorphic,
+  squareMatrixRotation,
+  uniqueArray,
+} from '.'
 
-function equalLists(l1: any[], l2: any[]) {
-  expect(JSON.stringify(l1)).toEqual(JSON.stringify(l2))
-}
+describe('equalLists', () => {
+  function equalLists(l1: any[], l2: any[]) {
+    expect(JSON.stringify(l1)).toEqual(JSON.stringify(l2))
+  }
 
-describe('getCurrentGame', () => {
   it('returns its identity given an empty list', () => {
     const input: number[] = []
     const output: number[] = []
@@ -33,10 +38,86 @@ describe('getCurrentGame', () => {
     equalLists(uniqueArray(input), output)
   })
 
-  it('removes repeted elements', () => {
+  it('removes repeated elements', () => {
     const input: number[] = [1, 1, 4, 5, 1, 4, 7, 8, 5]
     const output: number[] = [1, 4, 5, 7, 8]
 
     equalLists(uniqueArray(input), output)
+  })
+
+  it('removes repeted elements, including non natural numbers', () => {
+    const input: number[] = [6, 6, Infinity, 13, 2, 13, NaN]
+    const output: number[] = [6, Infinity, 13, 2, NaN]
+
+    equalLists(uniqueArray(input), output)
+  })
+})
+
+describe('isIsomorphic', () => {
+  it('equal strings are isomorphic', () => {
+    expect(isIsomorphic('code', 'code')).toBeTruthy()
+  })
+
+  it('strings with different lengths are not isomorphic', () => {
+    expect(isIsomorphic('eyes', 'eye')).toBeFalsy()
+  })
+
+  it('isomorphic strings', () => {
+    expect(isIsomorphic('paper', 'title')).toBeTruthy()
+  })
+
+  it('not isomorphic strings', () => {
+    expect(isIsomorphic('egg', 'sad')).toBeFalsy()
+  })
+})
+
+describe('decimalToBinary', () => {
+  it('base case', () => {
+    expect(decimalToBinary(0)).toBe('0')
+    expect(decimalToBinary(1)).toBe('1')
+  })
+
+  it('1 digit transformation', () => {
+    expect(decimalToBinary(3)).toBe('11')
+  })
+
+  it('Big number transformation', () => {
+    expect(decimalToBinary(1000)).toBe('1111101000')
+  })
+})
+
+describe('squareMatrixRotation', () => {
+  it('rotates a 3x3 matrix by 90 degrees', () => {
+    const input = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ]
+    const output = [
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3],
+    ]
+
+    expect(squareMatrixRotation(input)).toEqual(output)
+  })
+
+  it('rotates a 5x5 matrix by 90 degrees', () => {
+    const input = [
+      [6, 1, 4, 4, 7],
+      [3, 5, 8, 2, 10],
+      [0, 2, 6, 9, 4],
+      [6, 0, 1, 1, 26],
+      [3, 2, 2, 7, 9],
+    ]
+    const output = [
+      [3, 6, 0, 3, 6],
+      [2, 0, 2, 5, 1],
+      [2, 1, 6, 8, 4],
+      [7, 1, 9, 2, 4],
+      [9, 26, 4, 10, 7],
+    ]
+
+    expect(squareMatrixRotation(input)).toEqual(output)
   })
 })
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 // Removes duplicates of an array and returns an array of only unique elements.
+// Example:
 // [1, 2, 3, 5, 1, 5, 9, 1, 2, 8] => [1, 2, 3, 5, 9, 8]
 export function uniqueArray(array: number[]): number[] {
   const alreadyAdded: { [key: number]: boolean | undefined } = {}
@@ -12,4 +13,79 @@ export function uniqueArray(array: number[]): number[] {
   })
 
   return res
+}
+
+// For two strings to be isomorphic, all occurrences of a character in string A can be replaced with another character
+// to get string B. The order of the characters must be preserved. There must be one-to-one mapping for ever char of
+// string A to every char of string B.
+// Example:
+// `paper` and `title` => true
+// `egg` and `sad` => false
+// `dgg` and `add` => true
+export function isIsomorphic(firstString: string, secondString: string) {
+  if (firstString.length !== secondString.length) {
+    return false
+  }
+
+  const firstStringOnAscii = [...firstString].map(currChar =>
+    currChar[0].charCodeAt(0),
+  )
+  const secondStringOnAscii = [...secondString].map(currChar =>
+    currChar[0].charCodeAt(0),
+  )
+
+  const charsDistance = {}
+  let isValid = true
+
+  firstStringOnAscii.forEach((currChar, i) => {
+    const distance = firstStringOnAscii[i] - secondStringOnAscii[i]
+
+    if (!charsDistance[currChar]) {
+      charsDistance[currChar] = distance
+    } else {
+      if (charsDistance[currChar] !== distance) {
+        isValid = false
+      }
+    }
+  })
+
+  return isValid
+}
+
+// Write a recursive function that returns the binary string of a given decimal number
+// Example:
+// 3 => 11
+// 8 => '1000'
+// 1000 => '1111101000'
+export function decimalToBinary(digit: number) {
+  if (digit <= 1) {
+    return digit.toString()
+  }
+
+  const updatedDigit = Math.floor(digit / 2)
+
+  if (digit % 2 === 0) {
+    return decimalToBinary(updatedDigit) + '0'
+  } else {
+    return decimalToBinary(updatedDigit) + '1'
+  }
+}
+
+// You are given an n x n 2D matrix (representing an image). Rotate the matrix by 90 degrees (clockwise).
+// Example:
+// [                   [
+//   [1, 2, 3],          [7, 4, 1],
+//   [4, 5, 6],   =>     [8, 5, 2],
+//   [7, 8, 9],          [9, 6, 3],
+// ]                   ]
+export function squareMatrixRotation(originalMatrix: number[][]) {
+  const rotatedMatrix = originalMatrix.map((array, row) =>
+    array.map((_, column) => {
+      const x = originalMatrix.length - column - 1
+      const y = row
+      return originalMatrix[x][y]
+    }),
+  )
+
+  return rotatedMatrix
 }
